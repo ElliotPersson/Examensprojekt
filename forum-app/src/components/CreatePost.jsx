70af47d298ec0dbff121e4b3f6eceb80ad20edcd
Elliot Persson <elliot.thomas.persson@gmail.com>
2025-04-24 14:40:55 +0200

    add CreatePost form and connect to firestore, user can now create post

--- forum-app/src/components/CreatePost.jsx
+++ forum-app/src/components/CreatePost.jsx
@@ -1,22 +1,42 @@
 import { useState, useEffect } from "react";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 
 
 function CreatePost() {
 
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
-
+    async function handleSubmit(e) {
+        e.preventDefault();
+    
+        try {
+            await addDoc(collection(db, "posts"), {
+              title,
+              content,
+              createdAt: serverTimestamp()
+            });
+            
+            setTitle("");
+            setContent("");
+    } catch (error) {
+        console.error("Error with adding post:", error);
+    }
+ }
 
     return(<>
 
-        <form>
+        <form className="create-form" onSubmit={handleSubmit}>
 
-        <input type="text" />
+        <input type="text" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea placeholder="content" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+        <button type="submit">Create post</button>
 
-
-        </form>
+        </form> 
     
-    </>)
+    </>)    
 }
 
 export default CreatePost
